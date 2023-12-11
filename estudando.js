bdfc476a5f60440529196db3747049b30ffcb53c
Gisellebt <152410751+Gisellebt@users.noreply.github.com>
2023-12-11 10:22:04 -0300

    Update estudando.js

--- estudando.js
+++ estudando.js
@@ -1,1 +1,27 @@
-console.log("olá mundo!");
+let heroi = ("Heroi")
+let xp = ("1000")
+
+if ( xp <= 1000){
+ console.log("Seu XP é Ferro!")
+}
+
+if (1001 <= xp <= 2000){
+    console.log("Seu XP é Bronze")
+}
+if (2001 <= xp <= 5000){
+    console.log("Seu XP é Prata")
+}
+if (6001 <= xp <=7000){
+    console.log("Seu XP é Ouro!")
+}
+if (7001 <= xp <= 8000){
+    console.log("Seu XP é Platina diamante!")
+}
+if (8001 <= xp <= 9000){
+    console.log("Seu XP é Ascendente!")
+}
+if (9001 <= xp <=10000){
+    console.log("Seu XP é Imortal!")
+}
+else
+ console.log("Seu XP é Radiante!")
